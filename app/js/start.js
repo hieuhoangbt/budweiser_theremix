7808946613fcae983a11eba814748509c77b3306
ducvietsqt <ducvietsqt@gmail.com>
2016-12-25 01:38:51 +0700

    up fixbug1

--- app/js/start.js
+++ app/js/start.js
@@ -79,10 +79,13 @@ Tool.prototype.addVideo = function (video, end, type) {
 
 Tool.prototype.snapCamera = function (video, camera, err) {
     var self = this;
+    var isIE = detectIE();
     var errorCallback = function (e) {
-        // alert('PC không hỗ trợ Camera! :( ');
+        alert('PC không hỗ trợ Camera! :( ');
         err();
     };
+    if(isIE != false) return err();
+
     navigator.getUserMedia = navigator.getUserMedia ||
         navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia ||
@@ -337,10 +340,50 @@ function loadFont(success) {
     }, 3000);
     fontLoader.loadFonts();
 }
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    // Test values; Uncomment to check result …
+
+    // IE 10
+    // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+    // IE 11
+    // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+    // Edge 12 (Spartan)
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+    // Edge 13
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
 
 /*Ready document Window*/
 
 window.onload = function () {
+
 
     BudWeiser.beforeStart();
     /*Action Page init*/
@@ -549,7 +592,7 @@ window.onload = function () {
                     }
 
                     function sucessBase(source) {
-                        $('.link-snap').addClass('disable');
+                        $('.link-snap, .edit-controll').addClass('disable');
                         $('.loadder').hide();
                         var src_base64 = source;
                         var i = 4;
